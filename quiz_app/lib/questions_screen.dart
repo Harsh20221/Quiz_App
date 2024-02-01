15fd7dadad46e5e41aa1c9f045fd98c830c265b3
Harsh Kumar <harsh20221@gmail.com>
2024-02-02 02:40:11 +0530

    questions screen

---
+++ quiz_app/lib/questions_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/widgets.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+
+
+//* This sections contains common Doubts and Queries regarding this code
+/* ///?111111111111111  `State<QuestionsScreen> createState()` is a rule for a Flutter widget named `QuestionsScreen`. This rule says: "When you need to remember changes (like user input or data from the internet), use the instructions I'll provide in `createState()` to keep track of those changes.
+So, ///?`createState()` is like a memory box for the `QuestionsScreen` widget. It helps the widget remember things that can change over time. The actual instructions for what to remember and how to handle changes would be inside the `createState()` method, which is not shown in your selected code. */
+
+
+
+
+/*//?2222222222222222 in very simple terms, `super.key` is like a special tag or label you can give to your `QuestionsScreen` when you create it. 
+//?This `key` helps Flutter to identify that specific `QuestionsScreen` later on. It's like if you had a bunch of boxes and you put a unique label on one of them so you could find it easily later*/
