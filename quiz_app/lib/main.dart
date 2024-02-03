0ec6e7eabafc5c8e75ef57234a02f0c655f0513a
Harsh Kumar <harsh20221@gmail.com>
2024-02-03 20:05:01 +0530

    new if else method

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -3,19 +3,5 @@ import 'package:quiz_app/quiz.dart';
 /////import 'start_sceen.dart';
 void main() {
   runApp(Quiz()); //TODO: Plaease don't forget to call the class name here
-   /*  MaterialApp( 
-      ///! This is the old redundant alternative to the above code
-      home: Scaffold( 
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: const LinearGradient(
-              colors: [Color(0xFF480366), Color(0xFFA60AF4)],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-            ),
-          ),
-          child: Startscreen(Quiz())))));} //! This is how you will call the function class 
-
-
- */
+   
 }
