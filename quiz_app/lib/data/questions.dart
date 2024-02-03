0b883bbc1c3bfb42d09f0f683b82c19bfb2613be
Harsh Kumar <harsh20221@gmail.com>
2024-02-03 23:40:48 +0530

    CORRECTED QUESTIONS

--- quiz_app/lib/data/questions.dart
+++ quiz_app/lib/data/questions.dart
@@ -1,16 +1,60 @@
+//! This file contains the questions for the quiz
 import 'package:quiz_app/models/quizquestions.dart';
+///* THIS FILE CONTAINS THE QUESTIONS FOR THE QUIZ
+const questions =[  //! This is how you declare a list of Objects in Dart , 
+   //TODO: Do not forget to add = sign after the list name
 
-const questions =[  //! This is how you declare a list of Objects in Dart , \
-//TODO: Do not forget to add = sign after the list name
-quiz_questions(
-"What is the capital of France?",
-["New York", "London", "Paris", "Dublin"]
-),//TODO: Do not forget to add a comma at the end of the list
-quiz_questions(
-"What is the capital of India?",
-["New Delhi", "Mumbai", "Kolkata", "Chennai"] //! The options are written in the form of a list
-)
+         quiz_questions(
+    'What are the main building blocks of Flutter UIs?',
+    [
+      'Widgets',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  ),
+  quiz_questions('How are Flutter UIs built?', [
+    'By combining widgets in code',
+    'By combining widgets in a visual editor',
+    'By defining widgets in config files',
+    'By using XCode for iOS and Android Studio for Android',
+  ]),
+  quiz_questions(
+    'What\'s the purpose of a StatefulWidget?',
+    [
+      'Update UI as data changes',
+      'Update data as UI changes',
+      'Ignore data changes',
+      'Render UI that does not depend on data',
+    ],
+  ),
+  quiz_questions(
+    'Which widget should you try to use more often: StatelessWidget or StatefulWidget?',
+    [
+      'StatelessWidget',
+      'StatefulWidget',
+      'Both are equally good',
+      'None of the above',
+    ],
+  ),
+  quiz_questions(
+    'What happens if you change data in a StatelessWidget?',
+    [
+      'The UI is not updated',
+      'The UI is updated',
+      'The closest StatefulWidget is updated',
+      'Any nested StatefulWidgets are updated',
+    ],
+  ),
+  quiz_questions(
+    'How should you update data inside of StatefulWidgets?',
+    [
+      'By calling setState()',
+      'By calling updateData()',
+      'By calling updateUI()',
+      'By calling updateState()',
+    ],
+  ),    //! Do not forget to add comma at the end of the object          
 
-
-
+ //! The options are written in the form of a list
 ]; //TODO: Do not forget to add a semicolon at the end of the list
--- quiz_app/lib/models/quizquestions.dart
+++ quiz_app/lib/models/quizquestions.dart
@@ -1,3 +1,4 @@
+//! This File  Contains the questions for the quiz , Use the quiz_questions function inside questions.dart file to create questions 
 class quiz_questions {
   const quiz_questions(this.text , this.answers);
   final String text;
