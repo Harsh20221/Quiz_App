1090e436eb019ae2333e3ffa5299b45c16a22f01
Harsh Kumar <harsh20221@gmail.com>
2024-02-04 06:35:17 +0530

    Answerbutton

--- quiz_app/lib/answerbutton.dart
+++ quiz_app/lib/answerbutton.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-class AnswerButton extends StatelessWidget {
-  AnswerButton(this.Answer, this.onTap, {super.key});
-  final String Answer;
-  final void Function()
-      onTap; //! This is how you would define a parameter for constructor function in Dart
+class AnswerButton extends StatelessWidget{
+  AnswerButton({required this.Answertext , required this.onTap,super.key});  //! Make sure this is how you'll pass arguments 
+  //! to the constructor of the class , Make sure to enclose all the elememnts inside ({ }) first inside round brackets and then parenthesis 
+  final  String Answertext;  
+  final void Function() onTap;  //! Make sure to use void Function() instead of void Function
   @override
   Widget build(context) {
     return ElevatedButton(
       onPressed: onTap,
-      child: Text(Answer),
+      child: Text(Answertext),
       style: ElevatedButton.styleFrom(
           backgroundColor: Color.fromARGB(255, 33, 1, 95),
           foregroundColor: Colors.white,
@@ -18,3 +18,4 @@ class AnswerButton extends StatelessWidget {
     );
   }
 }
+ //TODO: Let's say in case your code is not working after a little  odification in the code , then ehat you should do is rewrite it from scratch 
